3c3ac346244d21bcc6190830daec236a599a7dbe
一维 <persenlee90@gmail.com>
2017-11-06 15:14:41 +0800

    spritekit game
    inital project

--- ZombieCongs/GameViewController.swift
+++ ZombieCongs/GameViewController.swift
@@ -16,15 +16,9 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+            let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+            scene.scaleMode = .aspectFill
+            view.presentScene(scene)
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
